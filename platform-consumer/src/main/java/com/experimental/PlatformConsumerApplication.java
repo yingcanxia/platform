2e749ea3cea23ebac5c230561c0dcb5b9e6b934c
冯啸迪 <fengxiaodi@51shebao.com>
2020-11-10 20:31:15 +0800

    ribbon负载均衡

--- platform-consumer/src/main/java/com/experimental/PlatformConsumerApplication.java
+++ platform-consumer/src/main/java/com/experimental/PlatformConsumerApplication.java
@@ -3,11 +3,21 @@ package com.experimental;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @SpringBootApplication
 public class PlatformConsumerApplication {
+
     public static void main(String[] args) {
         SpringApplication.run(PlatformConsumerApplication.class,args);
+    }
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
     }
 }
---
+++ platform-consumer/src/main/java/com/experimental/controller/RibbonHelloController.java
@@ -0,0 +1,21 @@
+package com.experimental.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+public class RibbonHelloController {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    @RequestMapping("/ribbon/call")
+    public String call(){
+        // 调用服务，service-provider为注册的服务名称
+        // LoadBalancerInterceptor会拦截调用并根据服务名找到对应的服务
+        String callServiceResult = restTemplate.getForObject("http://platform-provider/hello",String.class);
+        return callServiceResult;
+    }
+}
