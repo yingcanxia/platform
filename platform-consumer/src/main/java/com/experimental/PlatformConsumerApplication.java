2fb755e72380221605dc05ddd935473d951d98a3
yingcanxia <3042004295@qq.com>
2020-11-15 23:20:49 +0800

    更新customer启动类

--- platform-consumer/src/main/java/com/experimental/PlatformConsumerApplication.java
+++ platform-consumer/src/main/java/com/experimental/PlatformConsumerApplication.java
@@ -2,22 +2,23 @@ package com.experimental;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
-@SpringBootApplication
+@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
 public class PlatformConsumerApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(PlatformConsumerApplication.class,args);
-    }
 
     @Bean
     @LoadBalanced
     public RestTemplate restTemplate(){
         return new RestTemplate();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlatformConsumerApplication.class,args);
     }
 }
