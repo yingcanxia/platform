552216059ad01415ac4b56d73878e989b18bc4df
yingcanxia <3042004295@qq.com>
2021-01-17 11:16:09 +0800

    hystrix dashboard更新

--- platform-consumer/src/main/java/com/experimental/req/service/PlatformProducerService.java
+++ platform-consumer/src/main/java/com/experimental/req/service/PlatformProducerService.java
@@ -4,7 +4,7 @@ import com.experimental.hystrix.PlatformProducerHystrix;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@FeignClient(fallback = PlatformProducerHystrix.class)
+@FeignClient(name = "platform-provider" , fallback = PlatformProducerHystrix.class)
 public interface PlatformProducerService {
 
     @RequestMapping("/hello")
