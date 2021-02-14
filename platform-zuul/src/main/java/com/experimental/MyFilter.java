184bea854d2660b6fc2a14db61b56d65e180bbbc
yingcanxia <3042004295@qq.com>
2021-02-14 12:22:06 +0800

    zuul

---
+++ platform-zuul/src/main/java/com/experimental/MyFilter.java
@@ -0,0 +1,46 @@
+package com.experimental;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+
+@Component
+public class MyFilter extends ZuulFilter {
+    @Override
+    public String filterType() {
+        return "pre";
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return true;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        RequestContext ctx = RequestContext.getCurrentContext();
+        HttpServletRequest request = ctx.getRequest();
+        String token = request.getParameter("token");
+        System.out.println(token);
+        if (token == null){
+            ctx.setSendZuulResponse(false);
+            ctx.setResponseStatusCode(401);
+            try {
+                ctx.getResponse().getWriter().write("没有token");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+        return null;
+    }
+}
